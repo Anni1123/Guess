9d71b7a274bfc20a231bae27425c027131dcf1eb
Anni1123 <kumarianni1213@gmail.com>
2020-03-01 11:41:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/guess/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.guess;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+     int randomnumber;
+     public void guess(View view){
+         EditText editText=(EditText)findViewById(R.id.guessNum);
+         int guessInt=Integer.parseInt(editText.getText().toString());
+         if(guessInt > randomnumber){
+
+             Toast.makeText(this,"Larger",Toast.LENGTH_LONG).show();
+         } else if(guessInt  < randomnumber){
+             Toast.makeText(this,"Lower",Toast.LENGTH_LONG).show();
+         }
+        else{
+             Toast.makeText(this,"Equal",Toast.LENGTH_LONG).show();
+         }
+
+     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Random rand = new Random();
+        randomnumber=rand.nextInt(20)+1;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
