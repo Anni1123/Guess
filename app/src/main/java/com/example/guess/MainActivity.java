aaa235535dcbb56cb8225f3d879b16ba810531e5
Anni1123 <kumarianni1213@gmail.com>
2020-03-01 11:43:01 +0530

    Third Commit

--- app/src/main/java/com/example/guess/MainActivity.java
+++ app/src/main/java/com/example/guess/MainActivity.java
@@ -6,8 +6,6 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
